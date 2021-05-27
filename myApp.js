b7c509e984d265e546081509d9551500de2d1e25
Koji Okajima <koji970124@gmail.com>
2021-05-27 14:05:27 -0700

    q2

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -19,10 +20,8 @@ const personSchema = new mongoose.Schema({
   }
 })
 
-mongoose.model('Person', personSchema)
 
-
-let Person;
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
